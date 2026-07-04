70b4918e9cf765d2b7a29b5fe93a47a2e4633a03
alireza <alirezaeeii1385@gmail.com>
2026-07-04 11:10:59 -0700

    added board model

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Board(models.Model):
+    owner = models.models.ForeignKey(User, related_name="boards", on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
+
+
+    def __str__(self):
+        return self.title

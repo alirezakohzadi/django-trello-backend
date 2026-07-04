e3404215ba1c986df549c960bb838a77d8c82729
alireza <alirezaeeii1385@gmail.com>
2026-07-04 12:37:48 -0700

    added ListViewSet in lists app

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import List
+from .serializers import ListSrz
+
+
+class ListViewSet(viewsets.ModelViewSet):
+    serializer_class = ListSrz
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return List.objects.filter(board__owner=self.request.user)

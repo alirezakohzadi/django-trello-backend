bfa362d2c94111e680e28c263ca36b71ee5a6f2b
alireza <alirezaeeii1385@gmail.com>
2026-07-04 04:32:56 -0700

    updated urls in user app for jwt

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
-urlpatterns = {
-    path("create/"),
-}
+
+
+urlpatterns = [
+    path('create/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('update/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
+

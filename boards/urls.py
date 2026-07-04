4e9a459557ac9bac62e310034c063de464ad3121
alireza <alirezaeeii1385@gmail.com>
2026-07-04 11:28:38 -0700

    updated url name

--- boards/urls.py
+++ boards/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import BoardViewSet
 
 router = DefaultRouter()
-router.register("boards", BoardViewSet, basename="boards")
+router.register("api", BoardViewSet, basename="boards")
 
 urlpatterns = router.urls

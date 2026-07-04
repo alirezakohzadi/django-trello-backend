2e01235e223d1ac5206118efbd6a81d0c2e06972
alireza <alirezaeeii1385@gmail.com>
2026-07-04 12:38:04 -0700

    added lists url

--- trello/urls.py
+++ trello/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
     path('boards/', include("boards.urls")),
+    path('lists/', include("lists.urls")),
     
 ]

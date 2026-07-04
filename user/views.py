fabcabad1f2ed0d81104302cba8bc3cb98bebb42
alireza <alirezaeeii1385@gmail.com>
2026-07-04 05:42:58 -0700

    Update user views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import views
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+from .serializers import UserCreateSrz
+
+class CreateUserView(views.APIView):
+    permission_classes = [AllowAny]
+    def post(self ,request):
+        serializer = UserCreateSrz(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"maessage": "user created succesfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        

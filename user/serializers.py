1d77602385fae1781be51942d0772fe2576fdb4b
alireza <alirezaeeii1385@gmail.com>
2026-07-04 05:41:51 -0700

    Add user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -4,7 +4,27 @@ from django.contrib.auth.models import User
 
 
 class UserCreateSrz(serializers.ModelSerializer):
-
+    password2 = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ["id", "username", "password", "password2", "email"]
+        extra_kwargs = {"password": {"write_only" : True}}
+
+
+
+
+    def validate(self, data):
+
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError({"password": "password2 is not match with password1"})
+        return data    
+
+
+
+
+
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        user = User.objects.create_user(**validated_data)
+        return user

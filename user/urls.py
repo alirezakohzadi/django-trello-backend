acdb2f7dfd3d6963d709f28d4c71c72d1f9902e8
alireza <alirezaeeii1385@gmail.com>
2026-07-04 10:54:17 -0700

    added userloginview and userlogoutview

--- user/urls.py
+++ user/urls.py
@@ -4,11 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import CreateUserView
+from .views import CreateUserView, UserLoginView, UserLogout
 
 
 urlpatterns = [
     path("create/", CreateUserView.as_view(), name="user-register"),
+    path("login/", UserLoginView.as_view(), name="user-login"),
+    path("logout/", UserLogout.as_view(), name="user-logout"),
     path('create/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('update/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 

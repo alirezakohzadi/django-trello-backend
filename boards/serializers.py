1df722e75cf5c494caad57e61fa6b5c063981656
alireza <alirezaeeii1385@gmail.com>
2026-07-04 12:36:28 -0700

    fixed the problems

--- boards/serializers.py
+++ boards/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Board
+from lists.serializers import ListSrz
+
+
+
 
 class BoardSrz(serializers.ModelSerializer):
+    lists = ListSrz(many=True, read_only=True)
     class Meta:
         model = Board
-        fields = ["id", "title"]
+        fields = "__all__"
+        read_only_fields = ["owner"]

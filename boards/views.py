0a0271f4a80a08f9d4b578a99c6d31d3b9795bdc
alireza <alirezaeeii1385@gmail.com>
2026-07-04 11:26:55 -0700

    added BoardViewSet

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BoardSrz
+from .models import Board
 
-# Create your views here.
+class BoardViewSet(viewsets.ModelViewSet):
+    serializer_class = [BoardSrz]
+    permission_classes = [IsAuthenticated]
+
+
+    def get_queryset(self):
+        return Board.objects.filter(owner=self.request.user)
+    
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+

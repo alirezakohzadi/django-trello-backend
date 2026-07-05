13db116214ca5063628a46d7a7b62a7a06aa31db
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:06:30 -0700

    fixed the updaing problems

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from .serializers import BoardSrz
 from .models import Board
+from django.db.models import Q
+
+
+
+
 
 class BoardViewSet(viewsets.ModelViewSet):
     serializer_class = BoardSrz
@@ -10,9 +16,19 @@ class BoardViewSet(viewsets.ModelViewSet):
 
 
     def get_queryset(self):
-        return Board.objects.filter(owner=self.request.user)
+        return Board.objects.filter(
+            Q(owner=self.request.user) |
+            Q(members=self.request.user)
+        ).distinct()
     
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def perform_update(self, serializer):
+        board = self.get_object()
+
+        
+        if board.owner != self.request.user:
+            raise PermissionDenied("فقط مالک برد می‌تواند آن را ویرایش کند.")
+        serializer.save()

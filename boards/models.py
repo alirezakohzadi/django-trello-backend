50a7636a286bbf50c2efab20a1b2f44db4a68d79
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:05:55 -0700

    added members in board model

--- boards/models.py
+++ boards/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Board(models.Model):
     owner = models.ForeignKey(User, related_name="boards", on_delete=models.CASCADE)
     title = models.CharField(max_length=225)
+    members = models.ManyToManyField(User, related_name="joined_boards", blank=True)
 
 
     def __str__(self):

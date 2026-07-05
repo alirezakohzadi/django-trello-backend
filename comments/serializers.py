75867446368b8b585f04d1ef76e9dfdfa396621f
alireza <alirezaeeii1385@gmail.com>
2026-07-05 04:36:06 -0700

    fixed the problems

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,5 +6,5 @@ from .models import Comment
 class CommentSrz(ModelSerializer):
     class Meta:
         model = Comment
-        firlds = "__all__"
+        fields = "__all__"
         read_only_fields = ["auther"]

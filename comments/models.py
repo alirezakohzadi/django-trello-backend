e7d5830af5b5ee917d6cfad5cd3d83e4d77742b4
alireza <alirezaeeii1385@gmail.com>
2026-07-05 04:24:06 -0700

    created Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cards.models import Card
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comment(models.Model):
+    card = models.ForeignKey(Card, verbose_name="comments", on_delete=models.CASCADE)
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[0:30]

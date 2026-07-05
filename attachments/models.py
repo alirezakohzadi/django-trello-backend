7c8c632e43c860252e006b15745f222dd0e293a2
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:12:16 -0700

    created Attachment model

--- attachments/models.py
+++ attachments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cards.models import Card
 
-# Create your models here.
+
+class Attachment(models.Model):
+    card = models.ForeignKey(
+        Card,
+        related_name="attachments",
+        on_delete=models.CASCADE
+    )
+
+    file = models.FileField(upload_to="attachments/")
+
+    uploaded_at = models.DateTimeField(auto_now_add=True)

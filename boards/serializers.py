8d1923c69bf8d939af98d58748f92e88cff65667
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:05:25 -0700

    show user members

--- boards/serializers.py
+++ boards/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from .models import Board
 from lists.serializers import ListSrz
-
-
-
+from user.serializers import UserSrz
+from django.contrib.auth.models import User
 
 class BoardSrz(serializers.ModelSerializer):
+    owner = UserSrz(read_only=True)
+
+    members = UserSrz(
+        many=True,
+        read_only=True
+    )
+
+    member_ids = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        many=True,
+        write_only=True,
+        source="members"
+    )
+
     lists = ListSrz(many=True, read_only=True)
+
     class Meta:
         model = Board
         fields = "__all__"

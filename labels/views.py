92f48461cde95322a27f27480df457d4aedefa7a
alireza <alirezaeeii1385@gmail.com>
2026-07-05 10:05:10 -0700

    createdd LabelViewSet view

--- labels/views.py
+++ labels/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
+from .models import Label
+from .seializers import LabelSrz
 # Create your views here.
+
+
+
+
+class LabelViewSet(viewsets.ModelViewSet):
+    serializer_class = LabelSrz
+    permission_classes = [IsAuthenticated]
+
+
+    def get_queryset(self):
+        return Label.objects.filter(
+            board__owner = self.request.user
+        ).order_by("title")
+    
+    def perform_create(self, serializer):
+        boaed = serializer.validated_data["board"]
+
+
+        if boaed.owner != self.request.user:
+            raise PermissionDenied("شما اجازه ساخت برچسب برای این برد را ندارید.")
+        serializer.save()
+    

d5def4490fc34fb5b5c8e371f3a7024cea3618e4
alireza <alirezaeeii1385@gmail.com>
2026-07-05 02:27:07 -0700

    added Card model in careds app

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from lists.models import List
 
-# Create your models here.
+
+
+
+class Card(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=False)

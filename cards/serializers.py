dd58fecc2928a46085efb3f511247b7ab12c8764
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:42:59 -0700

    added attachments to show in response

--- cards/serializers.py
+++ cards/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Card
 from labels.seializers import LabelSrz
+from attachments.serializers import AttachmentSrz
+
+
+
+
 class CardSrz(serializers.ModelSerializer):
+    attachments = AttachmentSrz(many=True, read_only=True)
     labels = LabelSrz(many=True, read_only=True)
     class Meta:
         model = Card

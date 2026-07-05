668c056012904ab71458da3238088fddbc3f52fc
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:08:25 -0700

    view ro show the user detail

--- user/views.py
+++ user/views.py
@@ -4,20 +4,31 @@ from django.contrib.auth import authenticate
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
 from .serializers import UserCreateSrz, UserLoginSrz
 
 class CreateUserView(views.APIView):
-    permission_classes = [AllowAny]
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+        return [AllowAny()]    
+    
+    
     def post(self ,request):
         serializer = UserCreateSrz(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"maessage": "user created succesfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    def get(self, request):
+        serializer = UserCreateSrz(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
         
 
 

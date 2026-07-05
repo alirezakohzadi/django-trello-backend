8bf19f5a0d696d5a54ff5c5d4eedf9efe741142d
alireza <alirezaeeii1385@gmail.com>
2026-07-05 04:55:55 -0700

    created lable model

--- labels/models.py
+++ labels/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from boards.models import Board
 # Create your models here.
+
+
+
+class Label(models.Model):
+    board = models.ForeignKey(Board, related_name="labels", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    color = models.CharField(max_length=30)
+
+
+
+    def __str__(self):
+        return self.title

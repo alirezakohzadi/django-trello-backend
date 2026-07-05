047418b869e79c023c3fd5fb40580893029c4b40
alireza <alirezaeeii1385@gmail.com>
2026-07-05 11:13:43 -0700

    AttachmentViewSet  view

--- attachments/views.py
+++ attachments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Attachment
+from .serializers import AttachmentSrz
 
-# Create your views here.
+
+class AttachmentViewSet(viewsets.ModelViewSet):
+    serializer_class = AttachmentSrz
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Attachment.objects.filter(
+            card__list__board__owner=self.request.user
+        )

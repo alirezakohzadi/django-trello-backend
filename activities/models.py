74ec3ba6a7ce8c1bc849664b5a8838cd3d0b4941
alireza <alirezaeeii1385@gmail.com>
2026-07-06 02:13:16 -0700

    Activity model

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from boards.models import Board
 
-# Create your models here.
+
+class Activity(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    board = models.ForeignKey(
+        Board,
+        related_name="activities",
+        on_delete=models.CASCADE
+    )
+
+    action = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]

a7b8462a99fe77ce752fa1b86e3ba427be7ac1ff
alireza <alirezaeeii1385@gmail.com>
2026-07-06 02:44:20 -0700

    added django_filters

--- trello/settings.py
+++ trello/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "lists",
     "comments",
     "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -155,15 +156,41 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+
+
+
+
+REST_FRAMEWORK = {
+
+}
+
+
+
+
+
+
+
+
+
+
 # REST_FRAME_WORK
 
 REST_FRAMEWORK = {
    
     'DEFAULT_AUTHENTICATION_CLASSES': (
-       
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        
-    )
+    ),
+
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+
+    "PAGE_SIZE": 10,
  
 }
 

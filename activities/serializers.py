45fa386f1f104737f6246a1db2bbfd3aa761a7ce
alireza <alirezaeeii1385@gmail.com>
2026-07-06 02:43:10 -0700

    ActivitySrz to show  user result

--- activities/serializers.py
+++ activities/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Activity
 
+from boards.models import Board
+from boards.serializers import BoardMiniSrz
+from user.serializers import UserSrz
+
+
+# class BoardMiniSrz(ModelSerializer):
+#     class Meta:
+#         model = Board
+#         fields = ["id", "title"]
+
+
 
 class ActivitySrz(ModelSerializer):
+    user = UserSrz(read_only=True)
+    board = BoardMiniSrz(read_only=True)
     class Meta:
         model = Activity
         fields = "__all__"
+
+

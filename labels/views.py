362a96a0e0fa27618702f8f801a68dfdcbe986f9
alireza <alirezaeeii1385@gmail.com>
2026-07-06 02:22:52 -0700

    updated the Activity view

--- labels/views.py
+++ labels/views.py
@@ -1,29 +1,33 @@
-from rest_framework import viewsets 
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
 from .models import Label
 from .seializers import LabelSrz
-# Create your views here.
-
-
+from activities.models import Activity
 
 
 class LabelViewSet(viewsets.ModelViewSet):
     serializer_class = LabelSrz
     permission_classes = [IsAuthenticated]
 
-
     def get_queryset(self):
         return Label.objects.filter(
-            board__owner = self.request.user
+            board__owner=self.request.user
         ).order_by("title")
-    
+
     def perform_create(self, serializer):
-        boaed = serializer.validated_data["board"]
+        board = serializer.validated_data["board"]
 
+        if board.owner != self.request.user:
+            raise PermissionDenied(
+                "شما اجازه ساخت برچسب برای این برد را ندارید."
+            )
 
-        if boaed.owner != self.request.user:
-            raise PermissionDenied("شما اجازه ساخت برچسب برای این برد را ندارید.")
-        serializer.save()
-    
+        label = serializer.save()
+
+        Activity.objects.create(
+            user=self.request.user,
+            board=board,
+            action=f"Created label '{label.title}'"
+        )

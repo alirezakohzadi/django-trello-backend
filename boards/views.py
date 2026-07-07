3b121322a6154c5432e31928ec674aeaf86f71cd
alireza <alirezaeeii1385@gmail.com>
2026-07-07 05:06:32 -0700

    updated

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-from django.shortcuts import render
+from django.core.cache import cache
 from django.db.models import Q
 
 
@@ -12,13 +14,40 @@ from .models import Board
 
 
 
-
 class BoardViewSet(viewsets.ModelViewSet):
     serializer_class = BoardSrz
     permission_classes = [IsAuthenticated]
 
+
+    filter_backends = [
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+
+
     search_fields = ["title"]
     ordering_fields = ["title"]
+
+
+
+
+
+    def list(self, request, *args, **kwargs):
+        cache_key = f"boards_{request.user.id}"
+
+        data = cache.get(cache_key)
+
+        if data is not None:
+            return Response(data)
+
+        response = super().list(request, *args, **kwargs)
+
+        cache.set(cache_key, response.data, timeout=300)
+
+        return response
+
+
 
 
     def get_queryset(self):
@@ -28,13 +57,31 @@ class BoardViewSet(viewsets.ModelViewSet):
         ).distinct()
     
 
+
+
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+        cache.delete(f"boards_{self.request.user.id}")
+
+
+
 
     def perform_update(self, serializer):
         board = self.get_object()
 
-        
         if board.owner != self.request.user:
             raise PermissionDenied("فقط مالک برد می‌تواند آن را ویرایش کند.")
+
         serializer.save()
+        cache.delete(f"boards_{self.request.user.id}")
+
+
+        
+
+    def perform_destroy(self, instance):
+        if instance.owner != self.request.user:
+            raise PermissionDenied("فقط مالک برد می‌تواند آن را حذف کند.")
+
+        instance.delete()
+        cache.delete(f"boards_{self.request.user.id}")
--- cards/views.py
+++ cards/views.py
@@ -1,14 +1,16 @@
-from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework import viewsets
+
 
 from django_filters.rest_framework import DjangoFilterBackend
+from django.core.cache import cache
 
-
+from activities.models import Activity
 from .serializers import CardSrz
 from .models import Card
-from activities.models import Activity
 
 
 
@@ -39,7 +41,23 @@ class CardViewSet(viewsets.ModelViewSet):
         "title",
         
         ]
+    
 
+
+    def list(self, request, *args, **kwargs):
+        cache_key = f"cards_{request.user.id}"
+
+
+        data = cache.get(cache_key)
+
+        if data is not None:
+            return Response(data)
+    
+        response =  super().list(request, *args, **kwargs)
+        cache.set(cache_key, response.data, timeout=300)
+
+
+        return response
 
     def get_queryset(self):
         return Card.objects.filter(
@@ -58,21 +76,30 @@ class CardViewSet(viewsets.ModelViewSet):
         
 
         card = serializer.save()
+        cache.delete(f"cards_{self.request.user.id}")
+        cache.delete(f"boards_{self.request.user.id}")
 
         Activity.objects.create(
             user=self.request.user,
             board=list_obj.board,
             action=f"Created card '{card.title}'"
         )
+
         
 
-
     def perform_update(self, serializer):
-        card = self.get_object()
+        new_list = serializer.validated_data.get("list")
 
+        if new_list and new_list.board.owner != self.request.user:
+            raise PermissionDenied("شما اجازه انتقال کارت به این لیست را ندارید.")
+
+        card = self.get_object()
         old_list = card.list
 
         updated_card = serializer.save()
+
+        cache.delete(f"cards_{self.request.user.id}")
+        cache.delete(f"boards_{self.request.user.id}")
 
         if old_list != updated_card.list:
             Activity.objects.create(
@@ -80,3 +107,8 @@ class CardViewSet(viewsets.ModelViewSet):
                 board=updated_card.list.board,
                 action=f"Moved card '{updated_card.title}'"
             )
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache.delete(f"cards_{self.request.user.id}")
+        cache.delete(f"boards_{self.request.user.id}")

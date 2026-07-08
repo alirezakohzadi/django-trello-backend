ee39905d587302095fc16540810cce079ddcc232
alireza <alirezaeeii1385@gmail.com>
2026-07-08 06:07:58 -0700

    added Swagger

--- trello/urls.py
+++ trello/urls.py
@@ -18,6 +18,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,9 +35,10 @@ urlpatterns = [
     path('labels/', include("labels.urls")),
     path('attachments/', include("attachments.urls")),
     path('activities/', include("activities.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     
 ]
-
 
 
 urlpatterns += static(

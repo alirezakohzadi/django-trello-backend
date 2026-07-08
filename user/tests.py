544e20893a6b1f487ac0401bda9fb78a1127b39c
alireza <alirezaeeii1385@gmail.com>
2026-07-08 08:03:35 -0700

    fix user authentication tests

--- user/tests.py
+++ user/tests.py
@@ -1,21 +1,20 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
-from django.urls import reverse
 
 
 class UserAuthTest(APITestCase):
 
     def test_register_user(self):
 
-        url = "/user/register/"
-
-        data = {
-            "username": "testuser",
-            "email": "test@test.com",
-            "password": "12345678"
-        }
-
-        response = self.client.post(url, data)
+        response = self.client.post(
+            "/user/create/",
+            {
+                "username": "testuser",
+                "email": "test@test.com",
+                "password": "12345678",
+                "password2": "12345678"
+            }
+        )
 
         self.assertEqual(
             response.status_code,
@@ -25,22 +24,19 @@ class UserAuthTest(APITestCase):
 
     def test_login_user(self):
 
-        register_url = "/user/register/"
-
         self.client.post(
-            register_url,
+            "/user/create/",
             {
                 "username": "testuser",
                 "email": "test@test.com",
-                "password": "12345678"
+                "password": "12345678",
+                "password2": "12345678"
             }
         )
 
 
-        login_url = "/user/create/token/"
-
         response = self.client.post(
-            login_url,
+            "/user/create/token/",
             {
                 "username": "testuser",
                 "password": "12345678"
@@ -53,7 +49,14 @@ class UserAuthTest(APITestCase):
             status.HTTP_200_OK
         )
 
+
         self.assertIn(
             "access",
+            response.data
+        )
+
+
+        self.assertIn(
+            "refresh",
             response.data
         )
